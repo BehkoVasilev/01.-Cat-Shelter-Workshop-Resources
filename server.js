0461fd8c2be8157e76caec009d71a5a262a6fe14
BehkoVasilev <vencislavvsilev@gmail.com>
2023-01-15 15:53:34 +0200

    visualization of addBreed and addCat pages

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const fs = require('fs/promises')
-const fss = require('fs')
+const fs = require('fs/promises');
 const http = require('http');
 
 const cats = require('./cats.json');
-const { ALL } = require('dns');
+
 
 const server = http.createServer(async (req, res) => {
     res.writeHead(200, {
@@ -12,19 +11,37 @@ const server = http.createServer(async (req, res) => {
 
     if (req.url == '/') {
         const homePage = await readFile('./views/home.html');
+
         const catsHtml = await Promise.all(cats.map(cat => catTemplate(cat)));
         const result = homePage.replace('{{cats}}', catsHtml);
+
         res.write(result);
-    // } else if (/cats\/\d+\/edit/.test(req.url)) {
-    //     let catId = req.url.split('/')[2];
-    //     let cat = cats.find(x => x.id == catId);
-    //     // res.write(editCat(cat))
+    } else if (/cats\/\d+\/edit/.test(req.url)) {
+        const catId = req.url.split('/')[2];
+        const cat = cats.find(x => x.id == catId);
+
+        const editHtml = await catEditTemplate(cat, './views/editCat.html');
+        res.write(editHtml)
     } else if (req.url == '/content/styles/site.css') {
         res.writeHead(200, {
             'Content-Type': 'text/css',
         });
         const siteCss = await readFile('./content/styles/site.css');
         res.write(siteCss)
+    } else if (req.url == '/cats/content/styles/site.css') {
+        res.writeHead(200, {
+            'Content-Type': 'text/css',
+        });
+        const siteCss = await readFile('./content/styles/site.css');
+        res.write(siteCss)
+    }else if (req.url == '/cats/add-breed') {
+        const addBreed = await readFile('./views/addBreed.html');
+
+        res.write(addBreed);
+    } else if (req.url == '/cats/add-cat') {
+        const addCat = await readFile('./views/addCat.html');
+
+        res.write(addCat);
     } else {
         res.write(`
             <h1>404</h1>
@@ -35,10 +52,20 @@ const server = http.createServer(async (req, res) => {
 });
 
 function readFile(path) {
-    return fs.readFile(path, {encoding: 'utf-8'})
+    return fs.readFile(path, { encoding: 'utf-8' })
 }
 
-async function catTemplate(cat){
+async function catEditTemplate(cat, path) {
+    const html = await readFile(path);
+
+    const modifiedHtml = Object.keys(cat).reduce((result, key) => {
+        return result.replace(`{{${key}}}`, cat[key]);
+    }, html);
+
+    return modifiedHtml
+}
+
+async function catTemplate(cat) {
     const html = await readFile('./views/partials/cat.html');
 
     // let result = html.replace('{{name}}', cat.name);
